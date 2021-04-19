4350b1e91917d53c0391c20d0814ab9252ef86d9
lizh90 <lizzie@orangejellyfish.com>
2021-04-19 17:32:15 +0100

    Redirect to homepage after sign in

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router';
 import Form from '../components/Form';
+import { useAuth } from '../utils/hooks/useAuth';
 
 const SignIn = () => {
+  const userInfo = useAuth();
+  const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    setAuth(userInfo);
+  }, [userInfo])
+
+  if (auth.currentUser && Object.keys(auth.currentUser).length ) {
+    return <Redirect to="/" />
+  }
+
   return (
     <div className="App">
       <header className="App-header">
